30468f1c1e8dceda749353c0f7822fb4565147b5
Wojtek <wojtek.kostanski@gmail.com>
2018-02-12 23:12:42 +0100

    testing angular

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
 		root to: "dashboard#index"
 	resources :customers, only: [ :index ]
+	get "angular_test" => "angular_test#index"
 end
--- webpack/application.js
+++ webpack/application.js
@@ -1,3 +1,51 @@
-require ("application.css");
+require("application.css");
 require("bootstrap/dist/css/bootstrap.css");
-console.log("Hello world!");
+var coreJS          = require('core-js');
+var zoneJS          = require('zone.js');
+var reflectMetadata = require('reflect-metadata');
+var ng              = {
+  core:                   require("@angular/core"),
+  common:                 require("@angular/common"),
+  compiler:               require("@angular/compiler"),
+  forms:                  require("@angular/forms"),
+  platformBrowser:        require("@angular/platform-browser"),
+  platformBrowserDynamic: require("@angular/platform-browser-dynamic"),
+  router:                 require("@angular/router")
+};
+//
+var AngularTestComponent = ng.core.Component({
+  selector: "shine-angular-test",
+  template: '\
+  <h2 *ngIf="salutation">Hello {{salutation}}!</h2> \
+  <form> \
+    <div class="form-group"> \
+      <label for="name">Name</label> \
+      <input type="text" id="name" class="form-control" \
+             name="name" bindon-ngModel="salutation"> \
+    </div> \
+  </form> \
+  '
+}).Class({
+  constructor: function() {
+    this.salutation = null;
+  }
+});
+
+
+var AngularTestAppModule = ng.core.NgModule({
+  imports: [ ng.platformBrowser.BrowserModule, ng.forms.FormsModule ],
+  declarations: [ AngularTestComponent ],
+  bootstrap: [ AngularTestComponent ]
+})
+.Class({
+  constructor: function() {}
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var shouldBootstrap = document.getElementById("angular-test");
+  if (shouldBootstrap) {
+    ng.platformBrowserDynamic.
+      platformBrowserDynamic().
+      bootstrapModule(AngularTestAppModule);
+  }
+});
